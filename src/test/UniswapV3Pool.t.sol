6d7b97e26eabd38f1d7e92b57a1692aa79ed1069
Louis-Amas <amaslouis@protonmail.com>
2023-05-25 12:27:17 +0200

    feat: create uniswap-v3 pool

--- src/test/UniswapV3Pool.t.sol
+++ src/test/UniswapV3Pool.t.sol
@@ -2,18 +2,32 @@
 pragma solidity <0.9.0;
 
 import "forge-std/Test.sol";
-import "../../lib-0_7_6/v3-core/contracts/interfaces/IUniswapV3Factory.sol";
-import "../../lib-0_7_6/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
+import "forge-std/console.sol"; 
+import "../../lib-0_7_6/v3-core/contracts/interfaces/IUniswapV3Factory.sol"; 
+import "../../lib-0_7_6/v3-core/contracts/interfaces/IUniswapV3Pool.sol"; 
 
-contract UniswapV3PoolTest is Test{
-    IUniswapV3Factory factory;
-    IUniswapV3Pool pool;
+import "./mocks/ERC20Mock.sol"; 
 
-    bool transferInMintCallback = true;
-    bool flashCallbackCalled = false;
+contract UniswapV3PoolTest is Test { 
+    IUniswapV3Factory public factory; 
+    IUniswapV3Pool public pool; 
 
-    function setUp() public {
-        factory = IUniswapV3Factory(deployCode("UniswapV3Factory.sol:UniswapV3Factory"));
+    ERC20Mock public WETH; 
+    ERC20Mock public DAI; 
+
+    function setUp() public { 
+        WETH = new ERC20Mock("WETH", "WETH", 18); 
+        DAI = new ERC20Mock("DAI", "DAI", 18); 
+
+        factory = IUniswapV3Factory(deployCode("UniswapV3Factory.sol:UniswapV3Factory")); 
+        pool = IUniswapV3Pool(
+            factory.createPool(address(WETH), address(DAI), 500)
+        );
+        console.log('Contract created successfully');
+    }
+
+    function testAllGod() public {
+        assertEq(true, true);
     }
 
 }
