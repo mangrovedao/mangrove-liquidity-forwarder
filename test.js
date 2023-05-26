e144e8bb5f0fb34ab95cdbb3e7773f56dfee7fee
Louis-Amas <amaslouis@protonmail.com>
2023-05-26 16:36:20 +0200

    fix: improve test

--- test.js
+++ test.js
@@ -4,12 +4,19 @@ const sqrtToPrice = (sqrtPrice, baseDecimals, quoteDecimals) => {
 
   return price;
 }
-// DAI/WETH
+// USDC/WETH
 const price = sqrtToPrice(1873995297957243817768222563116177n, 6n, 18n);
 
 console.log(price);
 
 const computeAsks = (price, volume, baseDecimals, quoteDecimals) => {
+  const maxDecimals = baseDecimals > quoteDecimals ? baseDecimals : quoteDecimals;
+  const _volume =  (price * volume * 10n ** (maxDecimals - quoteDecimals)) / (10n ** quoteDecimals);
+
+  return _volume;
+}
+
+const computeAsksInQuote = (price, volume, baseDecimals, quoteDecimals) => {
   const maxDecimals = baseDecimals > quoteDecimals ? baseDecimals : quoteDecimals;
   const _volume =  (price * volume * 10n ** (maxDecimals - quoteDecimals)) / (10n ** quoteDecimals);
 
@@ -25,6 +32,8 @@ const computeBids = (price, volume, baseDecimals, quoteDecimals) => {
 
 const asks = computeAsks(price, 1787n * 10n**6n , 6n, 18n);
 const bids = computeBids(price, 1n * 10n**18n, 6n, 18n);
+const asksWithQuote = computeAsks(price, bids , 6n, 18n);
 
 console.log('asks', asks);
 console.log('bids', bids);
+console.log('asks with quote', asksWithQuote);
